9fca8601bc64461f24af1237a8882f55eeccc8d4
wankang <17611056063@163.com>
2020-01-02 09:23:30 +0800

    标题修改

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'web-flash Admin'
+const title = defaultSettings.title || '报修管理系统'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
