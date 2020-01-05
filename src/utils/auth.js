cb45dd5f9e3df8e845baa454c0df7cc96e6f2152
wankangldm <17611056063@163.com>
2020-01-05 22:59:58 +0800

    修改请求头

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,18 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'tokenHead'
 
 export function getToken() {
+  console.log("Cookie获取：[tokenHead: "+Cookies.get(TokenKey)+"]")
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
+  console.log("Cookies设置：[tokenHead: "+token+"]")
   return Cookies.set(TokenKey, token)
 }
 
 export function removeToken() {
+  console.log("Cookie获取：[tokenHead]")
   return Cookies.remove(TokenKey)
 }
